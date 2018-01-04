ece2f76961892c53843febb20e9a94bc1e5fd8ea
Ina Chemerko <innachemerko@gmail.com>
2018-01-04 02:16:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,40 +23,4 @@ function deepestChild(){
 
  }
      
-    /*if (deepChild.querySelectorAll('*')[i].childElementCount === 0) {
-      return deepChild.querySelectorAll('*')[i];*/
-  
- 
-    /*a(child);
-   
- return child.innerHTML;
-}
-
-function a(current){
-  while (current.childNodes){
-    var child = current.childNodes;
-    a(child);
-   
-  }
-  return child;
-  }
-
-
-/*function deepestChild(){
-  var current = document.getElementById('#grand-node');
-  console.log(current);
-  var next = [];
-  var deep;
-  while(current){
-    if(Array.isArray(current)){
-  for (var i = 0; i< current.length; i++){
-    next.push(current[i]);
-    console.log(next);
-    deep = current[i];
-    console.log(deep);
-  }}
-  current = next.shift();
-  }
-  console.log(deep);
-  return deep;
- } */
+    
