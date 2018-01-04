a0d2a9dbbb0e126df2167ad11d5500d7b6d6139b
Ina Chemerko <innachemerko@gmail.com>
2018-01-04 01:15:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,64 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested div.target');
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i< lis.length; i++){
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+  return lis;
+}
+
+function deepestChild(){
+  var current = document.querySelector('#grand-node');
+  while (current) {
+    if (current.children[0]) {
+      var deepChild = current.children[0];
+      console.log (deepChild);
+   }
+    current = current.children[0];
+    console.log(current);
+    }
+  console.log(current.innerHTML);
+ return deepChild.innerHTML;
+  
+}
+ 
+    /*a(child);
+   
+ return child.innerHTML;
+}
+
+function a(current){
+  while (current.childNodes){
+    var child = current.childNodes;
+    a(child);
+   
+  }
+  return child;
+  }
+
+
+/*function deepestChild(){
+  var current = document.getElementById('#grand-node');
+  console.log(current);
+  var next = [];
+  var deep;
+  while(current){
+    if(Array.isArray(current)){
+  for (var i = 0; i< current.length; i++){
+    next.push(current[i]);
+    console.log(next);
+    deep = current[i];
+    console.log(deep);
+  }}
+  current = next.shift();
+  }
+  console.log(deep);
+  return deep;
+ } */
